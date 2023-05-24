f385ebfb96ff030e0a08bfadf2d1146b2785d122
Nancy Du <nancyduyi@gmail.com>
2023-05-24 17:14:41 -0400

    fix live preview

--- sanity/lib/sanity.links.ts
+++ sanity/lib/sanity.links.ts
@@ -2,13 +2,9 @@ export function resolveHref(
   documentType?: string,
   slug?: string
 ): string | undefined {
-  if (slug === '/') {
-    return '/'
-  }
-
   switch (documentType) {
     case 'page':
-      return slug ? `/${slug}` : undefined
+      return slug
     default:
       console.warn('Invalid document type:', documentType)
       return undefined
